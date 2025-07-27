b84cbc1e0aa8119bb5d98de5a8521c91be9332e8
abhini1516 <abhini.s.amcec@gmail.com>
2025-07-27 19:52:27 +0530

    Fix: render frontend from Flask

--- app.py
+++ app.py
@@ -8,9 +8,13 @@ import os
 app = Flask(__name__, static_folder='static', template_folder='templates')
 CORS(app)
 
+
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 @app.route('/audio/<path:filename>')
 def serve_audio(filename):
